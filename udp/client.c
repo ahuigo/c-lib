4e73bd49a720d1fca45410fdc13aa538fa584d0a
hilojack <a132811@gmail.com>
2015-02-27 22:54:19 +0800

    c-lib init

---
+++ udp/client.c
@@ -0,0 +1,42 @@
+/*client.c*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "socklib.h"
+
+#define MAXLINE 80
+#define SERV_PORT 8000
+
+int main(int argc, char **argv) {
+	int servfd;
+	servfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	struct sockaddr_in servaddr;
+	socklen_t servaddr_len;
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
+	servaddr.sin_port = htons(SERV_PORT);
+
+	int n;
+	char buf[MAXLINE];
+	while(fgets(buf, MAXLINE, stdin)){
+		servaddr_len = sizeof(servaddr);
+		n = sendto(servfd, buf, strlen(buf), 0, (struct sockaddr *)&servaddr, servaddr_len);
+		if(n<0){
+			perr_exit("sendto error");
+		}
+		n = recvfrom(servfd, buf, MAXLINE, 0, NULL, 0);
+		if(n<0){
+			perr_exit("sendto error");
+		}
+		write(1, buf, n);
+	}
+	return 0;
+}
+
---
+++ udp/server.c
@@ -0,0 +1,43 @@
+/* server.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "socklib.h"
+
+#define MAXLINE 80
+#define SERV_PORT 8000
+
+int main(int argc, char **argv) {
+	int sockfd;
+	struct sockaddr_in servaddr,cliaddr;
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(SERV_PORT);
+
+	Bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+	printf("Accepting connections...\n");
+	char buf[MAXLINE];
+	char str[INET_ADDRSTRLEN];
+	int i,n;
+	socklen_t cliaddr_len;
+	while(1){
+		cliaddr_len = sizeof(cliaddr);
+		n = recvfrom(sockfd, buf, MAXLINE, 0, (struct sockaddr *)&cliaddr, &cliaddr_len);
+		if(n == -1)
+			perr_exit("recvfrom error");
+		printf("Received from %s as port %d \n", inet_ntop(AF_INET, &cliaddr.sin_addr, str, sizeof(str)), ntohs(cliaddr.sin_port));
+		for(i=0;i<n;i++){
+			buf[i] = toupper(buf[i]);
+		}
+		sendto(sockfd, buf, n, 0, (struct sockaddr *)&cliaddr, cliaddr_len);
+	}
+}
+
