92cdcb385f54037578fc100823e26e0d9e266613
hilojack <a132811@gmail.com>
2018-03-06 17:21:07 +0800

    hello

--- asm/hello-mac.asm
+++ asm/hello-mac.asm
@@ -1,26 +1,27 @@
 ; for x86-64 on mac osx!
-;	nasm -f macho64 -o asm1.o asm1.asm
-;	ld -o asm1 -e _main asm1.o
-;	./asm1
+;	nasm -f macho64 -o hello.o hello-mac.asm
+;	ld -o hello -e main hello.o
+;	./hello
 SECTION .data
 
 msg: db "hello xiaozi!", 0x0a
 len: equ $-msg
 
 SECTION .text
-global _main
+global main
 
 kernel:
     syscall
     ret
 
-_main:
-    mov rax,0x2000004
+main:
+	//test
+    mov rax,0x2000004;
     mov rdi,1
     mov rsi,msg
     mov rdx,len
     call kernel
 
     mov rax,0x2000001
-    mov rdi,0
+    mov rdi,0 ;//exit status
     call kernel
